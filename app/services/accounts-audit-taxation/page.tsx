b83825c38469181e5ae6dd1fa524d17ff52bc086
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-09-20 20:18:45 +0530

    Update page.tsx

--- app/services/accounts-audit-taxation/page.tsx
+++ app/services/accounts-audit-taxation/page.tsx
@@ -4,14 +4,14 @@ import AccountsBreadcrumb from "@/components/services/accounts&audit/AccountsBre
 import AccountsHero from "@/components/services/accounts&audit/AccountsHero";
 import AccountTaxationCards from "@/components/services/accounts&audit/AccountTaxation";
 
-export default function AccountsPage(){
-    return(
-        <>
-       <AccountsHero/>
-        <AccounntsBreadcrumb/>
-        <AccountingCards/>
-        <AccountAuditCards/>
-        <AccountTaxationCards/>
-        </>
-    )
+export default function AccountsPage() {
+  return (
+    <>
+      <AccountsHero />
+      <AccountsBreadcrumb />
+      <AccountingCards />
+      <AccountAuditCards />
+      <AccountTaxationCards />
+    </>
+  );
 }
