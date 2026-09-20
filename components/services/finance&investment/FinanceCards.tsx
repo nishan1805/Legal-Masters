1068758b5f995bd9481a6c723e73f3c881152aa2
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-09-20 19:37:01 +0530

    Update FinanceCards.tsx

--- components/services/finance&investment/FinanceCards.tsx
+++ components/services/finance&investment/FinanceCards.tsx
@@ -104,7 +104,7 @@ export default function FinanceCards() {
 
           {/* Enquire Now */}
           <Link
-            href={`https://wa.me/919589393949?text=${encodeURIComponent(
+            href={`https://wa.me/916262343456?text=${encodeURIComponent(
              `Hi Legal Masters, I would like to enquire about ${card.title}. Please assist me.`
             )}`}
            target="_blank"
