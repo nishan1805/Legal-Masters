ede0c2f05c4de9b5f3bf82aca81b99e20a209476
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-09-20 20:23:25 +0530

    Update FinanceHero.tsx

--- components/services/finance&investment/FinanceHero.tsx
+++ components/services/finance&investment/FinanceHero.tsx
@@ -1,15 +1,10 @@
-
 import Image from "next/image";
-import Navbar from "@/components/Navbar";
 
 export default function FinanceHero() {
   return (
     <header className="relative w-full overflow-hidden bg-[#174A8B]">
 
-      {/* Reusable Navbar */}
-      <Navbar />
-
-      {/* Background Artwork */}
+    {/* Background Artwork */}
       <div className="pointer-events-none absolute inset-0 z-0">
 
         {/* Book */}
